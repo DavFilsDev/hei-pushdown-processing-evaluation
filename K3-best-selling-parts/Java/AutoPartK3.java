15fd1af4d3c53ad49c87e8af949b25245fe42542
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-09 21:50:15 +0300

    feat(k3): add AutoPart entity for K3 exercise

---
+++ K3-best-selling-parts/Java/AutoPartK3.java
@@ -0,0 +1,31 @@
+package com.hei.k3;
+
+public class AutoPartK3 {
+    private int id;
+    private int carModelId;
+    private String serialNumber;
+    
+    public AutoPartK3() {}
+    
+    public AutoPartK3(int id, int carModelId, String serialNumber) {
+        this.id = id;
+        this.carModelId = carModelId;
+        this.serialNumber = serialNumber;
+    }
+    
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    
+    public int getCarModelId() { return carModelId; }
+    public void setCarModelId(int carModelId) { this.carModelId = carModelId; }
+    
+    public String getSerialNumber() { return serialNumber; }
+    public void setSerialNumber(String serialNumber) { this.serialNumber = serialNumber; }
+    
+    @Override
+    public String toString() {
+        return "AutoPartK3{id=" + id + ", carModelId=" + carModelId + 
+               ", serialNumber='" + serialNumber + "'}";
+    }
+}
