e00774f95f8263aca9ad2d73e92c011494ba9275
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-10 20:21:56 +0300

    review(k1-garage-repair): remove setters and getters in CarModel

--- K1-garage-repairs/java/CarModel.java
+++ K1-garage-repairs/java/CarModel.java
@@ -11,18 +11,5 @@ public class CarModel {
         this.model = model;
     }
     
-    // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    
-    public String getBrand() { return brand; }
-    public void setBrand(String brand) { this.brand = brand; }
-    
-    public String getModel() { return model; }
-    public void setModel(String model) { this.model = model; }
-    
-    @Override
-    public String toString() {
-        return "CarModel{id=" + id + ", brand='" + brand + "', model='" + model + "'}";
-    }
+   
 }
